d90ca1d4445f2e85e445551f232e8492198e9041
abas-dev <abasisaac22@gmail.com>
2023-03-05 05:40:02 +0100

    added auth views

--- authApp/views.py
+++ authApp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm  
+from django.contrib.auth.views import LoginView,LogoutView
+from django.contrib.auth import authenticate,login,logout
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+class UserLoginView(LoginView):
+    template_name = 'login.html'
+    next_page = ''
+
+class UserRegistrationView(CreateView):
+    template_name = 'register.html'
+    model =  UserCreationForm
+    
