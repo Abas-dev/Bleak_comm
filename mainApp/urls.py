82cfda32e0057a24d0bf22e35ef866e925a6ac8c
abas-dev <abasisaac22@gmail.com>
2023-03-08 04:02:46 +0100

    added the cart page and the detail page functionality

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import HomePageView,AboutPageView
+from .views import HomePageView,AboutPageView,ProductDetailView,CartPageView
 
 app_name = 'base'
 
 urlpatterns = [
     path('',HomePageView.as_view(), name='homePage'),
     path('about/',AboutPageView.as_view(),name='aboutPage'),
-    
+    path('product/<int:pk>',ProductDetailView.as_view(),name='productDetail'),
+    path('cart/',CartPageView.as_view(),name='cart'),
 ]
--- mainApp/views.py
+++ mainApp/views.py
@@ -11,9 +11,20 @@ class HomePageView(LoginRequiredMixin,ListView):
     model = Products
     context_object_name = 'product'
 
+    def get_context_data(self, **kwargs):
+        context =super().get_context_data(**kwargs)
+        context['category'] = Catergory.objects.all()
+        return context
+
 class AboutPageView(LoginRequiredMixin,TemplateView):
     template_name = 'about.html'
 
 class ProductDetailView(LoginRequiredMixin,DetailView):
     template_name = 'productDetail.html'
+    model = Products
+    context_object_name = 'productDes'
 
+class CartPageView(LoginRequiredMixin,TemplateView):
+    template_name = 'cart.html' 
+   
+       
